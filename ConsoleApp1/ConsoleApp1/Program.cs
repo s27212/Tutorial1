4129808ba50b4ade784ad82e341e362b34de8fe9
s27212 <s27212@pjwstk.edu.pl>
2024-03-18 16:01:34 +0100

    calculateAverage

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World3!");
+int[] arr = Enumerable.Range(1,10).ToArray();
+Console.WriteLine(calculateAvg(arr));
+
+static double calculateAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
